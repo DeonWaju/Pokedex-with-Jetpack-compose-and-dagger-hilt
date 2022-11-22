0ca8284da88e14a1d8021f7077df3c0eb0758f37
deonolarewaju <deonolarewaju@gmail.com>
2022-11-22 11:31:53 +0100

    worked on logo and search bar

--- app/src/main/java/com/example/pokedexappwithjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/pokedexappwithjetpackcompose/MainActivity.kt
@@ -3,20 +3,16 @@ package com.example.pokedexappwithjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.pokedexappwithjetpackcompose.app.pokemonList.PokemonListScreen
 import com.example.pokedexappwithjetpackcompose.ui.theme.PokedexAppWithJetpackComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,50 +22,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexAppWithJetpackComposeTheme {
-                val navController = rememberNavController()
-                NavHost(
-                    navController = navController,
-                    startDestination = "pokemon_list_screen"
-                ) {
-                    composable("pokemon_list_screen") {
-
-                    }
-                    composable("pokemon_details_screen/{dominantColor}/{pokemonName}",
-                        arguments = listOf(
-                            navArgument("dominantColor") {
-                                type = NavType.IntType
-                            },
-                            navArgument("pokemonName") {
-                                type = NavType.StringType
-                            }
-                        )
-                    ) {
-                        val dominantColor = remember {
-                            val color = it.arguments?.getInt("dominantColor")
-                            color?.let { Color(it)} ?: Color.White
-                        }
-                        val pokemon = remember {
-                            it.arguments?.getString("pokemonName")
-                        }
-
-                    }
-
-                }
-
+                Home()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Home() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "pokemon_list_screen"
+    ) {
+        composable("pokemon_list_screen") {
+            PokemonListScreen(navController = navController)
+        }
+        composable("pokemon_details_screen/{dominantColor}/{pokemonName}",
+            arguments = listOf(
+                navArgument("dominantColor") {
+                    type = NavType.IntType
+                },
+                navArgument("pokemonName") {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val dominantColor = remember {
+                val color = it.arguments?.getInt("dominantColor")
+                color?.let { Color(it) } ?: Color.White
+            }
+            val pokemon = remember {
+                it.arguments?.getString("pokemonName")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     PokedexAppWithJetpackComposeTheme {
-        Greeting("Android")
+        Home()
     }
 }
